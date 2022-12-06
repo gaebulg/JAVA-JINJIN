864d03186053664109f44c1b17ab683497a7fad4
gaebulg <103102042+gaebulg@users.noreply.github.com>
2022-12-07 00:44:09 +0800

    test
    test

---
+++ Java12/Class14.java
@@ -0,0 +1,8 @@
+package Java12;
+public class Class14 {
+    public static void main(String[] argv) {
+
+        String sentence="Habit is second nature.";
+        System.out.println(sentence.substring(16,22));
+    }
+}
